f865118676672ba38effa5be614df211809251dd
Iara <iarapou21@hotmail.com>
2023-01-05 16:35:37 -0300

    Crea función asignarEstilos
    Esta función se encarga de seleccionar las tarjetas y asignarle a cada una un pókemon del array declarado anteriormente.

--- src/script.js
+++ src/script.js
@@ -7,8 +7,19 @@ function inicializarjuego() {
     const pokemones = ["pickachu", "bulbasaur", "charmander", "caterpie", "pidgey", "squirtle"];
     const pokemonesDuplicados = pokemones.concat(pokemones);
     let pokemonesMezclados = mezclarArray(pokemonesDuplicados);
+
+    asignarEstilos(pokemonesMezclados);
+}
+
 function mezclarArray(array) {
     return array.sort(() => .5 - Math.random());
 }
+
+function asignarEstilos(pokemonesMezclados) {
+    $tarjetas.forEach(function (tarjeta, indice) {
+        tarjeta.classList.add(`${pokemonesMezclados[indice]}`);
+    })
+}
+
 }
 }

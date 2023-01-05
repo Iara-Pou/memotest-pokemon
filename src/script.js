563371c5bf41feee87aa79571abb6c737fb13b2f
Iara <iarapou21@hotmail.com>
2023-01-05 16:34:40 -0300

    Declara las clases de pokemones y las mezcla
    Primero, declaro un array. Lo duplico y después lo guardo mezclado, pasándolos por la función mezclarArray.

--- src/script.js
+++ src/script.js
@@ -4,5 +4,11 @@ const $tarjetas = $pantalla.querySelectorAll(".col");
 inicializarjuego();
 
 function inicializarjuego() {
+    const pokemones = ["pickachu", "bulbasaur", "charmander", "caterpie", "pidgey", "squirtle"];
+    const pokemonesDuplicados = pokemones.concat(pokemones);
+    let pokemonesMezclados = mezclarArray(pokemonesDuplicados);
+function mezclarArray(array) {
+    return array.sort(() => .5 - Math.random());
+}
 }
 }
